661a13f452a641b4709dafb707a9b34ed65e6954
OsafAliSayed <aousafalisayedb2@gmail.com>
2022-11-15 23:40:38 +0530

    15 November 2022

---
+++ Array/getminmax.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+from functools import reduce
+def getMinMax( a, n):
+    max = reduce(lambda x, y:  y if y > x else x, a)
+    min = reduce(lambda x, y: x if y > x else y, a)
+    return [max, min]
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        
+        product = getMinMax(a, n)
+        print(product, end=" ")
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# } Driver Code Ends
---
+++ Array/reverseastring.py
@@ -0,0 +1,11 @@
+# link
+# https://practice.geeksforgeeks.org/problems/reverse-an-array/0
+# code
+T = int(input())
+for i in range(T):
+    l = int(input())
+    arr = [int(i) for i in input().split()]
+    
+    for i in arr[::-1]:
+        print(i, end=" ")
+    print()
