97b49d434a6d657afb2b32512d6363344e78ef11
OsafAliSayed <aousafalisayedb2@gmail.com>
2022-11-17 13:42:44 +0530

    17 November 2022

---
+++ Array/segregateelements.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    def segregateElements(self, arr, n):
+        # Your code goes here
+        positive = list(filter(lambda x: x >= 0, arr))
+        negative = list(filter(lambda x: x <= 0, arr))
+        count = 0
+        for i in positive:
+            arr[count] = i
+            count += 1 
+        
+        for i in negative:
+            arr[count] = i
+            count += 1
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.segregateElements(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
---
+++ Array/sort012.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    def sort012(self,arr,n):
+        # code here
+        count = [0, 0, 0]
+        for i in arr:
+            if i == 0:
+                count[0] += 1
+            elif i == 1:
+                count[1] += 1
+            else:
+                count[2] += 1 
+        for i in range(count[0]):
+            arr[i] = 0
+        for i in range(count[0], count[0] + count[1], 1):
+            arr[i] = 1
+        for i in range(count[0] + count[1], count[0] + count[1] + count[2], 1):
+            arr[i] = 2
+            
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.sort012(arr,n)
+        for i in arr:
+            print(i, end=' ')
+        print()
+
+# } Driver Code Ends
